d4cf497ad74b95ce5f2afec3fdd861862d6af6de
Blue Roar <14856499+Blue-Roar@users.noreply.github.com>
2023-10-19 22:22:25 +0800

    Update main.js
    关闭语言转换

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         萌娘百科重定向
 // @namespace    https://github.com/Blue-Roar/moegirl-redirect
-// @version      0.3
-// @description  将萌娘百科移动版重定向到桌面版，繁中到简中，主站到镜像站（可选）
+// @version      0.4
+// @description  将萌娘百科移动版重定向到桌面版，繁中到简中（关闭语言转换），主站到镜像站（可选）
 // @author       Blue-Roar
 // @include      *://mzh.moegirl.org.cn/*
 // @include      *://zh.moegirl.org.cn/*
@@ -12,8 +12,8 @@
 
 const oldURL = document.location.href;
 var newURL = oldURL;
-newURL = newURL.replace('zh-tw', 'zh-cn').replace('zh-hk', 'zh-cn').replace('zh-sg', 'zh-cn');
 newURL = newURL.replace('mzh.moegirl.org.cn', 'zh.moegirl.org.cn');
+newURL = newURL.replace('zh-cn', 'zh').replace('zh-hk', 'zh').replace('zh-tw', 'zh').replace('zh-sg', 'zh').replace('zh-hans', 'zh').replace('zh-hant', 'zh');
 
 // 下面一行为镜像站跳转功能，如有需要可取消注释，亦可将脚本内的所有镜像站域名(moegirl.uk)替换成别的
 // newURL = newURL.replace('zh.moegirl.org.cn', 'moegirl.uk');
