2df8ec48481a6ecb42c3b60a21686f7a78046c60
Blue Roar <14856499+Blue-Roar@users.noreply.github.com>
2023-10-19 22:44:21 +0800

    Update main.js
    修改语言转换策略，默认简中

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         萌娘百科重定向
 // @namespace    https://github.com/Blue-Roar/moegirl-redirect
-// @version      0.5
-// @description  将萌娘百科移动版重定向到桌面版，繁中到简中（关闭语言转换），主站到镜像站（可选）
+// @version      0.6
+// @description  将萌娘百科移动版重定向到桌面版，语言转换（默认简体），主站到镜像站（可选）
 // @author       Blue-Roar
 // @match        *://mzh.moegirl.org.cn/*
 // @match        *://zh.moegirl.org.cn/*
@@ -12,8 +12,18 @@
 
 const oldURL = document.location.href;
 var newURL = oldURL;
+const targetLang = 'zh-hans'; //可修改为下方语言列表中的值
+/*
+    萌娘百科支持的语言列表
+    zh          关闭转换
+    zh-hans     简体
+    zh-hant     繁体
+    zh-cn       大陆简体
+    zh-hk       香港繁体
+    zh-tw       台湾繁体
+*/
 newURL = newURL.replace('mzh.moegirl.org.cn', 'zh.moegirl.org.cn');
-newURL = newURL.replace('zh-cn', 'zh').replace('zh-hk', 'zh').replace('zh-tw', 'zh').replace('zh-sg', 'zh').replace('zh-hans', 'zh').replace('zh-hant', 'zh');
+newURL = newURL.replace('zh-cn', targetLang).replace('zh-hk', targetLang).replace('zh-tw', targetLang).replace('zh-sg', targetLang).replace('zh-hans', targetLang).replace('zh-hant', targetLang);
 
 // 下面一行为镜像站跳转功能，如有需要可取消注释，亦可将脚本内的所有镜像站域名(moegirl.uk)替换成别的
 // newURL = newURL.replace('zh.moegirl.org.cn', 'moegirl.uk');
